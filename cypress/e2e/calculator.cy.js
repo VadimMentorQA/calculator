65dbfbcf34bd7ea8a7983fa532e821e1301b1b4d
v.savchenko <v.savchenko@polytech.software>
2023-09-07 20:43:15 +0300

    Refactor CI tests workflow, add web UI tests for calculator.cy.js, and update devDependencies in package.json

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -1,49 +1,43 @@
 /// <reference types="cypress" />
 
+describe("web UI tests - ", () => {
+  beforeEach(() => {
+    cy.visit("http://127.0.0.1:3000/index.html");
+  });
 
-describe('example to-do app', () => {
-    beforeEach(() => {
+  it("add 2 numbers", () => {
+    cy.get("#num1").type("1");
+    cy.get("#num2").type("2");
+    cy.get("#operation").select("add");
+    cy.get("#calculate").click();
+    cy.get("#result").should("have.text", "Result: 3");
+    console.log("add 2 numbers test passed");
+  });
 
-      cy.visit('http://127.0.0.1:3000/index.html')
-    })
-  
-    it('add 2 numbers', () => {
-        cy.get('#num1').type('1')
-        cy.get('#num2').type('2')
-        cy.get('#operation').select('add')
-        cy.get('#calculate').click()
-        cy.get('#result').should('have.text', 'Result: 3')
-        console.log('add 2 numbers test passed')
-    })
+  it("subtract 2 numbers", () => {
+    cy.get("#num1").type("1");
+    cy.get("#num2").type("2");
+    cy.get("#operation").select("subtract");
+    cy.get("#calculate").click();
+    cy.get("#result").should("have.text", "Result: -1");
+    console.log("subtract 2 numbers test passed");
+  });
 
-    it('subtract 2 numbers', () => {
-        cy.get('#num1').type('1')
-        cy.get('#num2').type('2')
-        cy.get('#operation').select('subtract')
-        cy.get('#calculate').click()
-        cy.get('#result').should('have.text', 'Result: -1')
-        console.log('subtract 2 numbers test passed')
-    }
-    )
+  it("multiply 2 numbers", () => {
+    cy.get("#num1").type("1");
+    cy.get("#num2").type("2");
+    cy.get("#operation").select("multiply");
+    cy.get("#calculate").click();
+    cy.get("#result").should("have.text", "Result: 2");
+    console.log("multiply 2 numbers test passed");
+  });
 
-    it('multiply 2 numbers', () => {
-        cy.get('#num1').type('1')
-        cy.get('#num2').type('2')
-        cy.get('#operation').select('multiply')
-        cy.get('#calculate').click()
-        cy.get('#result').should('have.text', 'Result: 2')
-        console.log('multiply 2 numbers test passed')
-    }
-    )
-
-    it('divide 2 numbers', () => {
-        cy.get('#num1').type('1')
-        cy.get('#num2').type('2')
-        cy.get('#operation').select('divide')
-        cy.get('#calculate').click()
-        cy.get('#result').should('have.text', 'Result: 0.5')
-        console.log('divide 2 numbers test passed')
-    }
-    )
-  })
-  
+  it("divide 2 numbers", () => {
+    cy.get("#num1").type("1");
+    cy.get("#num2").type("2");
+    cy.get("#operation").select("divide");
+    cy.get("#calculate").click();
+    cy.get("#result").should("have.text", "Result: 0.5");
+    console.log("divide 2 numbers test passed");
+  });
+});
